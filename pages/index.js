79f1a2a44e3191907d4d8096767b3b0bdde0b486
kelliewatson <kelliewatson@gmail.com>
2021-09-03 20:13:26 -0700

    test vercel with small text change

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
             href="https://nextjs.org/docs"
             className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+            <h3 className="text-2xl font-bold">Documentation test&rarr;</h3>
             <p className="mt-4 text-xl">
               Find in-depth information about Next.js features and API.
             </p>
