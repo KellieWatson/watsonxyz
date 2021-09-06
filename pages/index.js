48707c138f6e25ac07d5729cccb8d86513c65062
kelliewatson <kelliewatson@gmail.com>
2021-09-06 13:50:43 -0700

    CHanged WatsonXYZ into companyName variable

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
 import Head from 'next/head'
 
 export default function Home() {
+    const companyName = 'WatsonXYZ'
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>WatsonXYZ</title>
+        <title>{ `${companyName}`}</title>
         <link rel="icon" href="/Wxyz-larger.jpg" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
-          <a className="text-blue-900" href="https://watsonxyz-kelliewatson.vercel.app/">
-            WatsonXYZ!
+          <a className="bg-clip-text bg-gradient-to-r from-green-500 via-blue-500 to-purple-600 text-transparent" href="https://watsonxyz-kelliewatson.vercel.app/">
+              { `${companyName}!`}
           </a>
         </h1>
 
@@ -21,14 +22,14 @@ export default function Home() {
 
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t text-blue-800">
+      <footer className="flex items-center justify-center w-full h-24 border-t">
         <a
-          className="flex items-center justify-center"
+          className="flex items-center justify-center bg-clip-text bg-gradient-to-r from-green-500 via-blue-500 to-purple-600 text-transparent"
           href="https://watsonxyz-kelliewatson.vercel.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Developed by{' WatsonXYZ'}
+          Developed by{` ${companyName}`}
         </a>
       </footer>
     </div>
