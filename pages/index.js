132e7200c4dae00c2f87260ed72c95101c6bfb7b
kelliewatson <kelliewatson@gmail.com>
2021-09-06 14:59:10 -0700

    cleared out display change

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
       </Head>
 
         <header className="flex justify-end w-full h-24 border-b px-5 py-6">
+
             <div className="px-2">
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded">Login</button>
             </div>
