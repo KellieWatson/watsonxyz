1671d572a1663c2f3640dfde51d8cc1fdf952f12
kelliewatson <kelliewatson@gmail.com>
2021-09-06 19:22:13 -0700

    shortened about-us in header to about

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
                 <div className="px-2 py-2">Dashboard</div>
             <div className=" px-2 py-2">Finance</div>
             <div className=" px-2 py-2">Technology</div>
-            <div className="px-2 py-2">About-Us</div>
+            <div className="px-2 py-2">About</div>
             </div>
             <div className="flex justify-end">
             <div className="px-2">
