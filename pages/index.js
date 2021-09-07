766dec9ae29d10546e8570e18b9010a24f91c5fd
kelliewatson <kelliewatson@gmail.com>
2021-09-06 17:07:32 -0700

    last commit had errors in color schema. testing viability of color change on the footer companyName link

--- pages/index.js
+++ pages/index.js
@@ -48,7 +48,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
             <div>Developed by &nbsp;</div>
-            <div className="bg-gradient-to-r from-green-500 via-blue-500 to-purple-600 text-transparent bg-clip-text hover:from-pink-500 via-red-500 to-purple-600">{`${companyName}`}</div>
+            <div className="bg-gradient-to-r from-green-500 via-blue-500 to-purple-600 text-transparent bg-clip-text hover:from-pink-500 hover:via-red-500 hover:to-purple-600">{`${companyName}`}</div>
         </a>
       </footer>
     </div>
