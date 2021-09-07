38200a89c27f8dc02149cbe76f36ca71a3039726
kelliewatson <kelliewatson@gmail.com>
2021-09-06 17:04:09 -0700

    adjusted color of website name on hover

--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,15 @@ export default function Home() {
 
         <header className="grid grid-cols-2  w-full h-24 border-b px-5 py-6">
             <div className="flex justify-start text-xl font-semibold">
-            <div className="px-4 text-3xl font-bold bg-gradient-to-r from-green-500 via-blue-500 to-purple-600 text-transparent bg-clip-text">WatsonXYZ</div>
-            <div className="hidden px-2 py-2">Dashboard</div>
-            <div className="hidden px-2 py-2">Finance</div>
-            <div className="hidden px-2 py-2">Technology</div>
+            <div className="px-4 text-3xl font-bold bg-gradient-to-r from-green-500 via-blue-500 to-purple-600 text-transparent bg-clip-text hover:from-pink-500 via-red-500 to-purple-600">WatsonXYZ</div>
+                <div className="px-2 py-2">Dashboard</div>
+            <div className=" px-2 py-2">Finance</div>
+            <div className=" px-2 py-2">Technology</div>
             <div className="px-2 py-2">About-Us</div>
             </div>
             <div className="flex justify-end">
             <div className="px-2">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded">Login</button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded ">Login</button>
             </div>
             <div>
             <button className="hidden bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Register</button>
@@ -29,7 +29,7 @@ export default function Home() {
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
-          <a className="bg-clip-text bg-gradient-to-r from-green-500 via-blue-500 to-purple-600 text-transparent" href="https://watsonxyz-kelliewatson.vercel.app/">
+          <a className="bg-clip-text bg-gradient-to-r from-green-500 via-blue-500 to-purple-600 text-transparent hover:from-pink-500 via-red-500 to-purple-600" href="https://watsonxyz-kelliewatson.vercel.app/">
               { `${companyName}!`}
           </a>
         </h1>
@@ -48,7 +48,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
             <div>Developed by &nbsp;</div>
-            <div className="bg-gradient-to-r from-green-500 via-blue-500 to-purple-600 text-transparent bg-clip-text">{`${companyName}`}</div>
+            <div className="bg-gradient-to-r from-green-500 via-blue-500 to-purple-600 text-transparent bg-clip-text hover:from-pink-500 via-red-500 to-purple-600">{`${companyName}`}</div>
         </a>
       </footer>
     </div>
