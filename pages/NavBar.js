dff6e5e667f4c201eb191b7d393136ef1cbaa6b0
kelliewatson <kelliewatson@gmail.com>
2021-09-09 08:49:30 -0700

    Added menu behavior functionality, menu details are expanded when menu icon clicked, the view will then collapse when clicked again. The layout needs improvement. Also good idea to close expanded view when menu item is selected and new page view is displayed.

--- pages/NavBar.js
+++ pages/NavBar.js
@@ -1,11 +1,11 @@
 import Link from 'next/link'
+import {useState} from "react";
 
-
-function NavBar(props) {
-    const companyName = props.companyName;
+function NavBar({companyName}) {
+    const[menuView, setMenuView]=useState(false);
     const login = "Login";
     const register = "Register"
-    const onClickHandler = () => {console.log("Button pushed")};
+    const menuClicked = () => {setMenuView(!menuView); console.log(`Button Pressed: ${menuView}`);};
     return (
         <nav className="flex items-center flex-wrap border-b p-6">
             <Link href="/">
@@ -14,36 +14,39 @@ function NavBar(props) {
                    <span> {companyName}</span></a>
             </Link>
             <div className="inline-flex ml-auto lg:hidden">
-                <button onClick={onClickHandler} className="hover:outline-black">
+                <button onClick={menuClicked} className="hover:outline-black">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 md:h-10 md:w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
                 </button>
             </div>
-            <div className="hidden w-full lg:inline-flex lg:flex-grow lg:w-auto">
-                <div className="px-2 py-2 text-xl font-semibold">
-                    <Link href="/dashboard"><a>Dashboard</a>
-                    </Link></div>
-                <div className=" px-2 py-2 text-xl font-semibold">
-                    <Link href="/finance"><a>Finance</a></Link></div>
-                <div className=" px-2 py-2 text-xl font-semibold"><Link href="/technology"><a>Technology</a></Link>
-                </div>
-                <div className="px-2 py-2 text-xl font-semibold"><Link href="/about"><a>About</a></Link></div>
-                <div className="ml-auto flex justify-end">
-                    <div className="px-2">
-                        <Link href="/login" passHref>
-                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded ">Login</button>
-                        </Link>
+            <div className={`${menuView ? '':'hidden'} w-full lg:inline-flex lg:flex-grow lg:w-auto`}>
+                <div className="w-full lg:inline-flex lg:flex-grow lg:w-auto">
+                    <div className="px-2 py-2 text-xl font-semibold">
+                        <Link href="/dashboard"><a>Dashboard</a>
+                        </Link></div>
+                    <div className=" px-2 py-2 text-xl font-semibold">
+                        <Link href="/finance"><a>Finance</a></Link></div>
+                    <div className=" px-2 py-2 text-xl font-semibold"><Link href="/technology"><a>Technology</a></Link>
                     </div>
-                    <div>
-                        <Link href="/register" passHref>
-                            <button
-                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Register
-                            </button>
-                        </Link>
+                    <div className="px-2 py-2 text-xl font-semibold"><Link href="/about"><a>About</a></Link></div>
+                    <div className="ml-auto flex justify-end">
+                        <div className="px-2">
+                            <Link href="/login" passHref>
+                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded ">Login</button>
+                            </Link>
+                        </div>
+                        <div>
+                            <Link href="/register" passHref>
+                                <button
+                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Register
+                                </button>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
+
 
         </nav>
    /* <header className="grid grid-cols-2  w-full h-24 border-b px-5 py-6">
